284323a15f1d25497aa42d285b4027a022ddf2b0
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-13 11:47:29 +0530

    Added transition for List along with the item movement

--- src/components/UsersData.vue
+++ src/components/UsersData.vue
@@ -1,9 +1,9 @@
 <template>
-    <ul>
+    <transition-group tag="ul" name="user-data">
         <li v-for="user in users" :key="user" @click="removeUser(user)">
             {{ user }}
         </li>
-    </ul>
+    </transition-group>
     <input type="text" ref="userInput">
     <button @click="AddUser">Add User</button>
 </template>
@@ -31,7 +31,7 @@ export default {
 <style>
 ul {
     list-style: none;
-    margin:1rem 0;
+    margin: 1rem 0;
     padding: 0;
 }
 
@@ -39,5 +39,34 @@ li {
     border: 1px solid black;
     padding: 1rem;
     text-align: center;
+}
+
+.user-data-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.user-data-enter-active {
+    transition: all 1s ease-in;
+}
+
+.user-data-enter-to,
+.user-data-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.user-data-leave-active {
+    transition: all 1s ease-in;
+    position: absolute;
+}
+
+.user-data-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+.user-data-move {
+    transition: all 1s ease-in;
 }
 </style>
